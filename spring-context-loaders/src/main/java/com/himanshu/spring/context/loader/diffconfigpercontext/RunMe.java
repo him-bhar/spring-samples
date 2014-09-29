a279ec559a323939132a7b6c4d67bd9648a4c60d
Himanshu Bhardwaj <him_bhar@hotmail.com>
2014-09-29 22:11:24 +0530

    Added 2 different ways to load context
    2 ways to load context:
    1. 2 Config files, each config per context
    2. 1 config file for all context, the properties in config file is
    dynamically assigned to beans. Refer propertyPlaceHolderConfigurer
    extension.

---
+++ spring-context-loaders/src/main/java/com/himanshu/spring/context/loader/diffconfigpercontext/RunMe.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2013 Himanshu Bhardwaj
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+package com.himanshu.spring.context.loader.diffconfigpercontext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.himanshu.spring.context.common.DummyBean;
+
+public class RunMe {
+	private static Logger logger = LoggerFactory.getLogger(RunMe.class);
+	
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("classpath:/diff-config-per-context/context/applicationContext.xml");
+		ServiceFactoryDiffConfigPerContext svcRepo = appContext.getBean(ServiceFactoryDiffConfigPerContext.class);
+		DummyBean dummyBean_Context1 = svcRepo.getBean(DummyBean.class, "context1");
+		logger.info("From Dummy Bean in ctx1 : "+dummyBean_Context1.getWelcomeMsg());
+		DummyBean dummyBean_Context2 = svcRepo.getBean(DummyBean.class, "context2");
+		logger.info("From Dummy Bean in ctx2 : "+dummyBean_Context2.getWelcomeMsg());
+		appContext.destroy();
+	}
+}
