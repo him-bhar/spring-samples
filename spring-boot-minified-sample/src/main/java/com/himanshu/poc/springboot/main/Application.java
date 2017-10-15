da479d87d10e27f53e63442cbc9c0978543726aa
himanshu bhardwaj <himanshu_bhardwajin@yahoo.co.in>
2017-10-15 22:21:20 +0530

    1. Added swagger integration 2. Integrated spring security support on swagger and in app as general. 3. Added a rest api that serves xml as output.

--- spring-boot-minified-sample/src/main/java/com/himanshu/poc/springboot/main/Application.java
+++ spring-boot-minified-sample/src/main/java/com/himanshu/poc/springboot/main/Application.java
@@ -1,13 +1,17 @@
 package com.himanshu.poc.springboot.main;
 
+import com.himanshu.poc.springboot.config.SecurityConfig;
+import com.himanshu.poc.springboot.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages={"com.himanshu.poc.springboot.web.controller"})
+@SpringBootApplication(scanBasePackages = {"com.himanshu.poc.springboot.web.controller", "com.himanshu.poc.springboot.security"})
+@Import(value = {SwaggerConfig.class, SecurityConfig.class})
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
 }
--- spring-boot-minified-sample/src/main/java/com/himanshu/poc/springboot/web/controller/IndexController.java
+++ spring-boot-minified-sample/src/main/java/com/himanshu/poc/springboot/web/controller/IndexController.java
@@ -1,13 +1,49 @@
 package com.himanshu.poc.springboot.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.bind.annotation.XmlRootElement;
 
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
-    String index() {
-        System.out.println("IndexController3");
-        return "hello";
+  @RequestMapping("/index")
+  public String index() {
+    System.out.println("IndexController3");
+    return "hello";
+  }
+
+  @RequestMapping(value = "/index/api", method = RequestMethod.GET, produces = "application/xml")
+  public ResponseEntity<Person> api() {
+    Person p = new Person();
+    p.setFirstName("Himanshu");
+    p.setLastName("Tiger");
+    return new ResponseEntity<Person>(p, HttpStatus.OK);
+  }
+
+  @XmlRootElement
+  private static class Person {
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+      return firstName;
     }
+
+    public void setFirstName(String firstName) {
+      this.firstName = firstName;
+    }
+
+    public String getLastName() {
+      return lastName;
+    }
+
+    public void setLastName(String lastName) {
+      this.lastName = lastName;
+    }
+  }
 }
